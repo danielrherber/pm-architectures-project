d1bcbce8d2137b81830f3f41c422c2a34bca00b8
Daniel Herber <daniel.r.herber@gmail.com>
2024-01-25 19:50:01 -0700

    Temporary switch to py-networkx for isomorphism checking with single loops
    Fixes
    - networkx removed from_numpy_matrix (#5746), so now using from_numpy_array in detectiso_networkx.py
    
    Changes
    - Changed opts.isomethod to py-networkx for PMA_EX_A000041, PMA_EX_A053419, PMA_EX_A054921, and PMA_EX_A191970 to address issue #8

--- src/iso/python/detectiso_networkx.py
+++ src/iso/python/detectiso_networkx.py
@@ -9,8 +9,8 @@ def detectiso(adj1, adj2, node_color1, node_color2, n1, n2):
     adj1 = np.reshape(adj1, [n1,n1])
     adj2 = np.reshape(adj2, [n2,n2])
 
-    g1 = nx.from_numpy_matrix(adj1,create_using=nx.MultiGraph())
-    g2 = nx.from_numpy_matrix(adj2,create_using=nx.MultiGraph())
+    g1 = nx.from_numpy_array(adj1,create_using=nx.MultiGraph())
+    g2 = nx.from_numpy_array(adj2,create_using=nx.MultiGraph())
 
     for x in range(0, n1):
         g1.nodes[x]['color'] = node_color1[x]
